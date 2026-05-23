e1efaf47db3357e27350bbf1b652f8d8b7fb64de
BingBangBoom1212 <bwpt859g44@privaterelay.appleid.com>
2026-05-23 02:17:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const API_SECRET = process.env.API_SECRET || "";
 const linkedAccounts = {
   "3321828490": "1265951794891329561"
   "5028632040": "1400599067624607784"
+  "4036042012": "1357929023363219598"
+
+
 };
 
 app.get("/", (req, res) => {
